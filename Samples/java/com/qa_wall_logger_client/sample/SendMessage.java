83364db30147deb78ccad9984f97e2ad8e18ca6a
Martin Moreno <155martinmoreno@gmail.com>
2015-11-10 18:07:21 -0300

    Changed: Project Structure;

---
+++ Samples/java/com/qa_wall_logger_client/sample/SendMessage.java
@@ -0,0 +1,46 @@
+package com.qa_wall_logger_client.sample;
+
+
+import com.google.gson.Gson;
+import com.qa_wall_logger_client.RemoteLogger;
+import com.qa_wall_logger_client.log.Log;
+
+public class SendMessage
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        //Remote Logger instantiation;
+
+        RemoteLogger remoteLogger = new RemoteLogger(new RemoteLogger.Listener()
+        {
+            @Override
+            public String onParseToJson(final Log log)
+            {
+                return new Gson().toJson(log);
+            }
+
+            @Override
+            public void onSentToNetwork(final String parsedObject)
+            {
+                //Send data to server here
+            }
+        });
+
+
+        // .... your code ....
+
+
+        //Remote Logger usage
+
+        //Send Period
+        Log periodStart = new Log(Log.LogType.PERIOD_START, System.currentTimeMillis(), "Hello World!");
+        remoteLogger.send(periodStart);
+
+        Log periodEnd = new Log(Log.LogType.PERIOD_END, System.currentTimeMillis()+ 5000, "Bye Bye World!");
+        remoteLogger.send(periodEnd);
+
+        //Send time
+        Log timeLog = new Log(Log.LogType.EVENT,  System.currentTimeMillis() ,"http://www.google.com");
+        remoteLogger.send(timeLog);
+    }
+}
