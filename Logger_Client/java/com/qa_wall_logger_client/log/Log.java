477af5529de5e939ebec414295aa4208a76bd549
Martin Moreno <155martinmoreno@gmail.com>
2015-11-11 12:29:52 -0300

    Changed: model to include an id;

--- Logger_Client/java/com/qa_wall_logger_client/log/Log.java
+++ Logger_Client/java/com/qa_wall_logger_client/log/Log.java
@@ -1,40 +1,37 @@
 package com.qa_wall_logger_client.log;
 
-public class Log
-{
-    public enum Type
-    {
+public class Log {
+    public enum Type {
         PERIOD_START, PERIOD_END, EVENT
     }
 
+    private String id;
     private final Type type;
     private long time;
     private String message;
 
     /**
+     * @param id      String representing the event (ie: if you are sending a START-END event, both of them will be tied by this id)
      * @param type    {@link Type Type} indicating the log type;
      * @param time    Epoch (milliseconds since 1 jan 1970) when the period start;
      * @param message a message to identify the com.qa_wall_logger_client.log;
      */
-    public Log(final Type type, long time, String message)
-    {
+    public Log(final String id, final Type type, long time, String message) {
+        this.id = id;
         this.type = type;
         this.time = time;
         this.message = message;
     }
 
-    public Type getType()
-    {
+    public Type getType() {
         return type;
     }
 
-    public long getTime()
-    {
+    public long getTime() {
         return time;
     }
 
-    public String getMessage()
-    {
+    public String getMessage() {
         return message;
     }
 }
--- Samples/java/com/qa_wall_logger_client/sample/SendMessage.java
+++ Samples/java/com/qa_wall_logger_client/sample/SendMessage.java
@@ -33,14 +33,14 @@ public class SendMessage
         //Remote Logger usage
 
         //Send Period
-        Log periodStart = new Log(Log.Type.PERIOD_START, System.currentTimeMillis(), "Hello World!");
+        Log periodStart = new Log("Test1", Log.Type.PERIOD_START, System.currentTimeMillis(), "Hello World!");
         remoteLogger.send(periodStart);
 
-        Log periodEnd = new Log(Log.Type.PERIOD_END, System.currentTimeMillis()+ 5000, "Bye Bye World!");
+        Log periodEnd = new Log("Test1", Log.Type.PERIOD_END, System.currentTimeMillis()+ 5000, "Bye Bye World!");
         remoteLogger.send(periodEnd);
 
         //Send time
-        Log timeLog = new Log(Log.Type.EVENT,  System.currentTimeMillis() ,"http://www.google.com");
+        Log timeLog = new Log("Test2", Log.Type.EVENT,  System.currentTimeMillis() ,"http://www.google.com");
         remoteLogger.send(timeLog);
     }
 }
